ce25807f2e021da8f5811248ed16566964afbfb1
jayasingh24 <96876047+jayasingh24@users.noreply.github.com>
2022-01-25 21:07:19 +0530

    two calculator at same page

--- calculator2.js
+++ calculator2.js
@@ -1,22 +1,75 @@
-function dis(val)
-{
-document.getElementById("display").value+=val;
- }
+if(container1.hasFocus()){
+    let cal1= container1.onclick(dis1(val));
 
- function solve()
-{
-let x = document.getElementById("display").value;
-let y = eval(x);
-document.getElementById("display").value = y;
-};
-function clr()
-{
-document.getElementById("display").value = "";
 }
 
-function back(){
-    let bc=document.getElementById("display").value;
+
+function dis1(val)
+{
+document.getElementById("display1").value+=val;
+ }
+ 
+ function solve1()
+
+    {
+let x = document.getElementById("display1").value;
+let y = eval(x);
+document.getElementById("display1").value = y;
+
+};
+
+
+
+
+
+function clr1(){
+        
+document.getElementById("display1").value = "";
+
+}
+
+
+function back1(){
+    let bc=document.getElementById("display1").value;
     temp=bc.slice(0,-1);
-    document.getElementById("display").value=temp;
+    document.getElementById("display1").value=temp;
+
+}
+
+
+//  cont2
+ if(container2.hasFocus()){
+    let cal1= container2.onclick(dis2(val));
+
+}
+function dis2(val)
+{
+document.getElementById("display2").value+=val;
+ }
+
+ function solve2()
+
+ {
+let x = document.getElementById("display2").value;
+let y = eval(x);
+document.getElementById("display2").value = y;
+
+};
+
+
+
+
+
+function clr2(){
+     
+document.getElementById("display2").value = "";
+
+}
+
+
+function back2(){
+ let bc=document.getElementById("display2").value;
+ temp=bc.slice(0,-1);
+ document.getElementById("display2").value=temp;
 
 }
